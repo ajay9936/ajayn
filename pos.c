442cafbf00c8b2ecf228ec1b018328777aa511f9
ajay9936 <n.ajay9936@icloud.com>
2016-10-22 07:12:16 +0530

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+double number;
+printf("\n Enter the number:",number);
+scanf("%lf",&number);
+ if(number>0)
+   printf("\nThe number %lf is a positive number.",number);
+else if(number<0)
+   printf("\nThe number %lf is a negative number.",number);
+else
+   printf("\nThe entered number %lf is neither a positive number nor negative number.",number);
+return 0;
+}
