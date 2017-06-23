d5e3155647924636b87c342dbab8aa9360cd8dad
ajay9936 <n.ajay9936@icloud.com>
2017-06-23 12:21:44 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner n=new Scanner(System.in);
+		int num=n.nextInt();
+		int fact=1;
+		for(int i=1;i<=num;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+
+}
