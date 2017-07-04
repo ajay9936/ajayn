4b685a0769202bfd09cf090bda089be4713c2a11
ajay9936 <n.ajay9936@icloud.com>
2017-07-04 08:37:28 +0530

    Add files via upload

---
+++ Register.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Register {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of students");
+		int num=sc.nextInt();
+		int a[]=new int[num];
+		System.out.println("Enter "+num+" register numbers");
+		for(int i=0;i<num;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		Arrays.sort(a);
+		for(int i=0;i<num;i++)
+		{
+				if(a[i]>=0)
+				{
+			if(a[i]==a[(i+1)%num])
+				System.out.println("Dupicate number is "+a[i]);
+		}
+				else
+				{
+					System.out.println("You have entered a negative register number");
+				}
+		}
+		sc.close();
+	}
+}
